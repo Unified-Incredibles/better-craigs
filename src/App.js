76d5da4d21d363177498f34d4caeb5a15f4bb989
pradeepa24 <sh.pradeepa01@gmail.com>
2019-11-22 15:41:51 -0500

    added data

--- src/App.js
+++ src/App.js
@@ -2,13 +2,21 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/nav-bar-component/nav-bar';
 import DisplayPicturesList from './components/display-pictures-list-component/display-pictures-list';
+import data from './data.json';
 
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      pictureList:data
+    }
+   
+  }
   render() {
     return (
       <div>
         <NavBar />
-        <DisplayPicturesList />
+        <DisplayPicturesList pictureList = {this.state.pictureList}/>
       </div>
     );
   }
--- src/components/display-pictures-list-component/display-pictures-list.js
+++ src/components/display-pictures-list-component/display-pictures-list.js
@@ -2,10 +2,43 @@ import React, { Component } from 'react';
 import './display-pictures-list.css';
 
 export default class DisplayPicturesList extends Component {
+    displayData = () => {
+        return this.props.pictureList.map((picture,ind) => {
+            return (
+                <div key={ind} className="picture-content">
+                    <h3>{picture.Title}</h3>
+                    <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                     <div class="carousel-inner">
+                     {this.displayImages(picture.Images)}
+                      </div>
+                      <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                       <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                   <span class="sr-only">Previous</span>
+             </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+          <span class="carousel-control-next-icon" aria-hidden="true"></span>
+             <span class="sr-only">Next</span>
+       </a>
+</div>
+                </div>
+            )
+        })
+    }
+    displayImages = (imageList) => {
+        console.log("images")
+        return imageList.map((image,index) =>{
+            console.log("images"+image)
+            return (
+                <div key={index} className="carousal-self-item">
+                <img className="d-block w-100" src={image} alt="First slide" />
+                </div>
+            )
+        })
+    }
     render() {
         return (
             <div className="display-pictures-list-content">
-                
+                {this.displayData()}
             </div>
         )
     }
