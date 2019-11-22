9ea66d6846b51ae4ba66213359a1c062cfef767f
pradeepa24 <sh.pradeepa01@gmail.com>
2019-11-22 15:03:10 -0500

    added 1

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/nav-bar-component/nav-bar';
 import DisplayPicturesList from './components/display-pictures-list-component/display-pictures-list';
-function App() {
-  return (
-    <div>
-      <NavBar />
-      <DisplayPicturesList />
-    </div>
-  );
+
+class App extends React.Component{
+  render() {
+    return (
+      <div>
+        <NavBar />
+        <DisplayPicturesList />
+      </div>
+    );
+  }
+
 }
 
 export default App;
