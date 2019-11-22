424c50ef291f02f528e82707c15f612330caf13b
pradeepa24 <sh.pradeepa01@gmail.com>
2019-11-22 14:31:34 -0500

    added styles

--- better-craigs/src/App.js
+++ better-craigs/src/App.js
@@ -1,12 +1,28 @@
 import React from 'react';
 import './App.css';
-
+import profileIcon from './account-icon-check.png';
 function App() {
   return (
     <div>
-      <header>
-        
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-light">
+  <a className="navbar-brand" href="#">craigslist</a>
+  <div className="nav-middle">
+
+  </div>
+  <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+  </button>
+
+  <div className="collapse navbar-collapse" id="navbarSupportedContent">
+    <form className="form-inline my-2 my-lg-0">
+      <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
+      <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    </form>
+    <button className="profile"><img src={profileIcon} /></button>
+  </div>
+</nav>
+      {/* <header>
+      </header> */}
     </div>
   );
 }
---
+++ better-craigs/src/components/display-pictures-list-component/display-pictures-list.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './display-pictures-list.css';
+
+export default class DisplayPicturesList extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
