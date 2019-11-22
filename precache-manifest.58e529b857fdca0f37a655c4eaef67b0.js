25b41f2a06341c3020a69d65d0e78de46dfb4eac
pradeepa24 <sh.pradeepa01@gmail.com>
2019-11-22 14:32:07 -0500

    Updates

---
+++ precache-manifest.58e529b857fdca0f37a655c4eaef67b0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d4866f35a29bca821f4733ac0023579",
+    "url": "/better-craigs/index.html"
+  },
+  {
+    "revision": "dada025bf474be08cd7b",
+    "url": "/better-craigs/static/css/2.833dd627.chunk.css"
+  },
+  {
+    "revision": "f4e2f0a836cdfdb2b8a3",
+    "url": "/better-craigs/static/css/main.4689e8cb.chunk.css"
+  },
+  {
+    "revision": "dada025bf474be08cd7b",
+    "url": "/better-craigs/static/js/2.0e498a9b.chunk.js"
+  },
+  {
+    "revision": "f4e2f0a836cdfdb2b8a3",
+    "url": "/better-craigs/static/js/main.03826348.chunk.js"
+  },
+  {
+    "revision": "5c222c81cd42c275ec8b",
+    "url": "/better-craigs/static/js/runtime-main.0fe8a2c5.js"
+  },
+  {
+    "revision": "ef20e4111ab10ef9c79ecfe7e1633899",
+    "url": "/better-craigs/static/media/account-icon-check.ef20e411.png"
+  }
+]);
